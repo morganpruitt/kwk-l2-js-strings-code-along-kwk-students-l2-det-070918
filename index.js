c696ff39bb1f219fc11ffaac26fe6e47cef905ec
morganpruitt <2003morgannicole@gmail.com>
2018-07-11 17:35:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 // Write your code in this file!
-
+var currentUser = "motgab"  
+var welcomeMessage = "Welcome to Klossybook, " + currentUser + "!"
+var excitedWelcomeMessage = welcomeMessage.toUpperCase();
+var shortGreeting = "Welcome, " + currentUser.charAt(0) + "!"
